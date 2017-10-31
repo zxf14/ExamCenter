ec70b81ac8a4b07f1776b284135ed74e09ad237c
Camilla-NJU <guhan.1996@qq.com>
2017-10-31 17:02:19 +0800

    spring boot

---
+++ src/main/java/com/nju/coursework/TestCenterApplication.java
@@ -0,0 +1,13 @@
+package com.nju.coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCenterApplication.class, args);
+	}
+
+}
