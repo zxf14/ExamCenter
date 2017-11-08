b492a074fa14b8c65ea610275d021f9471f6e189
zxf14 <1048043763@qq.com>
2017-11-08 18:59:58 +0800

    .

--- src/main/java/com/nju/coursework/saas/db/UserRepository.java
+++ src/main/java/com/nju/coursework/saas/db/UserRepository.java
@@ -1,6 +1,5 @@
 package com.nju.coursework.saas.db;
 
-import com.nju.coursework.saas.entity.Student;
 import com.nju.coursework.saas.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
