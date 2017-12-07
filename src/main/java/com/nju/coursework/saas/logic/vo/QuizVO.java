e82e8c81af23cec2dbe5c2b3728d34b250a6989b
ZhangShiqi <zhangshiqi@maxtropy.com>
2017-12-07 17:05:30 +0800

    fix #263387 进入testing阶段

--- src/main/java/com/nju/coursework/saas/logic/vo/QuizVO.java
+++ src/main/java/com/nju/coursework/saas/logic/vo/QuizVO.java
@@ -5,10 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class QuizVO {
     private Integer value;
+    private List<String> optionId;
+    private int id;
 }
--- src/main/java/com/nju/coursework/saas/web/controller/ExamController.java
+++ src/main/java/com/nju/coursework/saas/web/controller/ExamController.java
@@ -1,6 +1,7 @@
 package com.nju.coursework.saas.web.controller;
 
 import com.nju.coursework.saas.logic.service.ExamConfigService;
+import com.nju.coursework.saas.logic.service.SubmitExamService;
 import com.nju.coursework.saas.logic.vo.QuestionVO;
 import com.nju.coursework.saas.logic.vo.QuizVO;
 import com.nju.coursework.saas.logic.vo.StudentVO;
@@ -17,37 +18,46 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ConfigExam")
+@RequestMapping("/Exam")
 public class ExamController {
 
     @Autowired
     private ExamConfigService examConfigService;
 
-    @PostMapping("/create")
+    @Autowired
+    private SubmitExamService submitExamService;
+
+    @PostMapping("/config/create")
     public String createExam(int questionNum, @NonNull List<Integer> scores, @RequestParam List<StudentVO> testees,
                              List<QuestionVO> questions, @NonNull Timestamp startTime, @NonNull Timestamp endTime,
                              HttpSession session) {
-         GeneralResponse response = examConfigService.examConfig((Integer)session.getAttribute("userId"),questionNum, scores,
-                 testees, questions, startTime, endTime);
-         return JsonUtil.toJsonString(response);
+        GeneralResponse response = examConfigService.examConfig((Integer) session.getAttribute("userId"), questionNum, scores,
+                testees, questions, startTime, endTime);
+        return JsonUtil.toJsonString(response);
     }
 
-    @PostMapping("/import")
+    @PostMapping("/config/import")
     public String importStudentToCreateExam(int quizCount, @RequestParam("file") MultipartFile file, String groupName,
                                             List<Integer> scores, List<QuestionVO> questions, Timestamp startTime, Timestamp endTime, HttpSession session) {
         GeneralResponse response;
         try {
 
-            response = examConfigService.examConfigByExcel((Integer)session.getAttribute("userId"), quizCount,
-                    file.getInputStream(),groupName, scores, questions, startTime, endTime);
+            response = examConfigService.examConfigByExcel((Integer) session.getAttribute("userId"), quizCount,
+                    file.getInputStream(), groupName, scores, questions, startTime, endTime);
         } catch (IOException e) {
             e.printStackTrace();
             return JsonUtil.toJsonString(new GeneralResponse(false, "excel文件导入出错"));
         }
         return JsonUtil.toJsonString(response);
     }
+
+    @PostMapping("/submit")
+    public String submitExam(int testeeId, List<QuizVO> quiz) {
+        GeneralResponse response = submitExamService.submitExam(testeeId, quiz);
+        return JsonUtil.toJsonString(response);
+    }
+
 }
