2bdf49d2180cd9d0d0d7136d4b8f61fed35aa5d3
zxf14 <1048043763@qq.com>
2017-12-15 20:25:52 +0800

    exam controller

--- src/main/java/com/nju/coursework/saas/web/controller/ExamController.java
+++ src/main/java/com/nju/coursework/saas/web/controller/ExamController.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/test/Exam")
+@RequestMapping("/test/exam")
 public class ExamController {
 
     @Autowired
