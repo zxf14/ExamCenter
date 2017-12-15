93a9c482ea82832376f0bf6ce7c3061cca1b4a90
zxf14 <1048043763@qq.com>
2017-12-15 17:02:42 +0800

    testee method

--- src/main/java/com/nju/coursework/saas/web/controller/TesteeController.java
+++ src/main/java/com/nju/coursework/saas/web/controller/TesteeController.java
@@ -7,6 +7,7 @@ import com.nju.coursework.saas.web.annotation.LoginRequired;
 import com.nju.coursework.saas.web.response.GeneralResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,7 +24,7 @@ public class TesteeController {
     TesteeService testeeService;
 
     @LoginRequired
-    @PostMapping("/list")
+    @GetMapping("/list")
     @ResponseBody
     public String getTesteesReport(int examId, HttpSession session) throws IOException {
         List<TesteeVO> testeeVOS = testeeService.getTesteeReport(examId);
