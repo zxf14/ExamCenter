1ebee607de663036b83299df080d633e9d0ebfef
zxf14 <1048043763@qq.com>
2018-01-05 21:52:57 +0800

    fix js

--- src/main/java/com/nju/coursework/saas/web/controller/TestController.java
+++ src/main/java/com/nju/coursework/saas/web/controller/TestController.java
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by guhan on 17/11/1.
  */
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping(value = {"/teacher/**", "/student/**","/login/**", "/"})
 public class TestController {
     @Autowired
     TestService testService;
 
     @GetMapping("/")
-    public String test() {
+    public String index() {
         return "index.html";
     }
 }
